8e2188228a0c5e4679fe5817f7d9040ef6a30972
FatherRtd <79792471+FatherRtd@users.noreply.github.com>
2024-05-16 19:49:24 +0500

    Задание 6.2

--- app/src/main/java/com/example/newapp/MainActivity.kt
+++ app/src/main/java/com/example/newapp/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.newapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnAllow: Button = findViewById(R.id.btnAllow)
+        val btnShow: Button = findViewById(R.id.btnShow)
+        val btnClose: Button = findViewById(R.id.btnClose)
+
+        btnAllow.setOnClickListener {
+            btnShow.isEnabled = true
+        }
+
+        btnShow.setOnClickListener {
+            btnClose.visibility = View.VISIBLE
+        }
+
+        btnClose.setOnClickListener {
+            finish()
         }
     }
 }
